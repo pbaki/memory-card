1901b893d7bcbabfd5072f24207b3143a0189aaf
pbaki <pbaki999@gmail.com>
2023-10-09 23:57:48 +0200

    Giphy API call for gifs to be randomized

--- src/memory-card.jsx
+++ src/memory-card.jsx
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react";
 
 function MemoryCard() {
   const [cards, setCards] = useState([]);
+  const [gameMode, setGameMode] = useState(0);
   useEffect(() => {
-    // Api call then select few random images/gifs as cards
-  });
+    async function getGifs() {
+      const response = await fetch(
+        "https://api.giphy.com/v1/gifs/search?api_key=mMtQHgFTpMeJ16MX4IkYVtqrMhVChGNa&q=cheeseburgers"
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+    }
+    getGifs();
+    return () => {};
+  }, []);
   return (
     <div className="gameContainer">
       <div className="statsContainer"></div>
